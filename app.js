6e78b26b01238c972784a0cb3cc58956e18a1201
wokoman <woko@aon.at>
2014-11-08 18:22:43 +0100

    Adaption

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-'use strict';
 
 var app = angular.module('firebaseIDXDemo', ['firebaseIDX']);  // reference to Modul 'firebaseIDX' in firebaseindex.js
 
@@ -11,7 +10,8 @@ var firebaseRef="https://fireindex.firebaseio.com/";
 app.controller('appCtrl', ['$scope','firebaseIndex', '$timeout',  // use Service 'firebaseIndex' from Modul 'firebaseIDX'
     
 function ($scope, firebaseIndex, $timeout) {
-        
+    'use strict';        
+    
     var person={
         lastname: "Smith",
         firstname: "Frank",
@@ -38,66 +38,74 @@ function ($scope, firebaseIndex, $timeout) {
         }
     ];
           
-    // set References to DATA and INDEX 
+    // set independent References to DATA and INDEX 
     var refData = new Firebase(firebaseRef + "persons");       
     var refIDX = new Firebase(firebaseRef + "IDX");        
     
-    // Start INDEXING here
-    firebaseIndex.indexOn(refData, indexOn,refIDX);
+    // Create an Index Object 
+    var FirebaseIdx=firebaseIndex(refData, indexOn, refIDX);
+    // var FirebaseIdx=firebaseIndex(refData, indexOn); // take Default refIDX
+    // ident -> var FirebaseIdx=new firebaseIndex(refData, indexOn [,refIDX]);
     
-    // add a Person to Firebase and watch Result in Chrome with Vulcane
-    refData.push(person);            
+    // // Start automatic INDEXING here...
+    FirebaseIdx.indexOn();
     
+    // // add a Person to Firebase and watch Result in Chrome with Vulcane
+    // refData.push(person);            
     
     //-------------------------------------------------------------------------------------
-    
-    
-    $scope.person=person;
-    
-    
-    $scope.queryLastname=function (term) {
-      
-        $scope.person.adress.street="";
         
-        var index={index: 'lastname',textMapper: lowerCaseFN}; // alternativ use indexOn[0]
-        var from=term;
-        var to=term+'\uFFFF';
-        var sortUp=true;
-
-        // query returns a promise
-        firebaseIndex.queryFromTo(index , from, to, sortUp)
-        .then( function(result){
-          $timeout(function(){$scope.persons=result; });  // instead of scope.$watch(...)                         
-        });
+    $scope.person=person;
+    $scope.indexOn=indexOn;
     
-    };
-
-    
-    $scope.queryStreetOfAdress=function (term) {
+    $scope.query=function (term, index, sortup) {
       
-        $scope.person.lastname="";
-         
+        if(index === undefined || term.length<1) {
+            $scope.persons=[];
+            return;
+        }
+        
+        // index -> {index: 'lastname',textMapper: lowerCaseFN}; // alternativ use indexOn[0]
+        var termTo=term+'\uFFFF';
+
         // query returns a promise
-        firebaseIndex.queryFromTo(indexOn[1] , term, term+'\uFFFF', true)
+        FirebaseIdx.queryFromTo(index , term, termTo, sortup)
         .then( function(result){
           $timeout(function(){$scope.persons=result; });  // instead of scope.$watch(...)                         
         });
     
     };
     
+    
+    $scope.onIDX=function () {
+
+       FirebaseIdx.indexOn();
+
+    };
+    
+    $scope.offIDX=function () {
+
+       FirebaseIdx.indexOff();
+
+    };
     
     $scope.recreateIDX=function () {
-        
-       firebaseIndex.indexRecreate();
+
+       FirebaseIdx.indexRecreate();
+
+    };
     
+    $scope.deleteIDX=function () {
+
+       FirebaseIdx.indexDelete();
+
     };
 
-    
     $scope.generatePersons=function () {
         
         var i;
         
-        for (i=0; i < 100; i++) {
+        for (i=0; i < 10; i=i+1) {
 
             // Randomize names
             $scope.person.lastname = Math.random().toString(36).slice(6);
--- firebaseindex.js
+++ firebaseindex.js
@@ -2,162 +2,214 @@
 
     'use strict';
 
-    // Angular Modul firebaseIDX  with Service firebaseIndex 
+    // Modul firebaseIDX  with Service firebaseIndex 
     var firebase = angular.module('firebaseIDX', []);
 
 /*
-    //  Sample Person
-    {
-        name: "Smith",
+// 0) Declare a Sample-Person
+
+    var person={
+        lastname: "Smith",
         firstname: "Frank"
         email: "frank.smith@fb.com",
         telefon:"12345-887766",
         adress: {
-            street: 'Darlington Road 12',
+            street: 'Arlington Road 12',
             location: 'Seattle',
             state: 'US'
         }
     };
 
-// 1) Declare your TextMapper ->  Function(term) {... modify your term...;  return modifiedTerm;} 
-// Sample textMapper function 
-    function upperCaseFN(term) {
+// 1) Declare your TextMapperFN ->  Function(term, textMapperArguments) {... modify your term...;  return modifiedTerm;} 
+
+// Sample for a TextMapper Function 
+
+    function upperCaseFN(term, textMapperArguments) {
+        if(typeof term === 'object') return term[textMapperArguments];
         if(typeof term === 'string') return term.toUpperCase();
+        //... convert your term in a priority you need for searching 
+        
         return null;        
     }
 
 // 2) Declare your Index Array for all the Properties you want to build an index 
+
 var indexOn = [
     {
-        index: 'name',
-        textMapper: upperCaseFN     // converts your value of Index into uppercase/ lowercase/ or any searchable and sortable term 
-                                    // this term will be written to priority.  
-                                    // https://www.firebase.com/docs/web/api/firebase/setpriority.html 
+        index: 'lastname',
+        textMapperFN: upperCaseFN,      // converts your value of Index into uppercase/ lowercase/ or any searchable and sortable mappedTerm 
+                                        // this mappedTerm will become a priority in your dataset.  
+                                        // (https://www.firebase.com/docs/web/api/firebase/setpriority.html) 
+                                        //
+                                        // Function Signatur:  mappedTerm = textMapperFN(term, textMapperArguments) 
+                                        // finally your dataset will looks like:
+                                        // Priority <- mappedTerm
+                                        // Name <- FirebaseKey
+                                        // Value <- term
     },
     {
         index: 'firstname',
-        textMapper: upperCaseFN
+        textMapperFN: upperCaseFN         // search will be not case sensitiv
     },
     {
         index: 'telefon',
-        textMapper: compactNumberFN     // mapping to trim and extract non numeric values
+        // textMapperFN: compactNumberFN     // mapping to trim and extract non numeric values
     },
     {
         index: 'email',
-        textMapper: getMailProviderFN
+        // textMapperFN: getMailProviderFN   // make only the provider searchable
     },
     {
-        index: 'adresse',
-        textMapper: firstPropertyOfObjectFN
+        index: 'adress',
+        textMapperFN: upperCaseFN      // mappedTerm = textMapperFN(term, textMapperArguments)
+        textMapperArguments: 'street'
     },
     {
-        index: 'adresse/Strasse',
-        textMapper: null            // no mapping wanted
+        index: 'adress/street',
+        textMapperFN: null                // no mapping wanted 
     }
 ];
 
 // 3) Reference to your Data Firebase
-var refData = new Firebase(https://YourFirebaseAccount.firebaseio.com/persons");     
+
+var refPersons = new Firebase(https://[Your Firebase Account].firebaseio.com/persons");     
 
 // 4) Reference to your Index Firebase (a Reference as child of refData is not allowed)
-var refIndex = new Firebase(https://YourFirebaseAccount.firebaseio.com/IDX");        
+// Your Index can be referenced to another Firebase Account
 
-// 5) Activate Indexing 
-firebaseIndex.indexOn(refPersons, indexOn, refIndex)
+var refIndex = new Firebase(https://[Your Firebase Account].firebaseio.com/IDX");        
 
-// ) that's it! every access (set, update, remove) to your Firebase refData will automatic update the Index  
+// 5) Create IndexObject and activate Indexing 
 
+var fbIdx=firebaseIndex(refPersons, indexOn, refIndex)
+
+// use your Index-Object and switch Indexing ON
+
+fbIdx.indexOn()             - start Indexing   
+// fbIdx.indexOff()         - stop Indexing
+// fbIdx.indexRecreate()    - if your Index is corrupt or not updated then recreate your Index 
+// fbIdx.indexDelete()      - delete the Index
+
+// 6) That's it! 
+// Every Access (Set, Update, Remove) to your Firebase 'refPersons' will automatically update your IDX  
+// try to add a dataset 
+refPersons.push(person);  
 
 Searching:
 
 var sortUp=true;
-var valueFrom="S";
-var valueTo="S"+'\uFFFF';
-var index=indexOn[0];  // { index: 'name', textMapper: upperCaseFN }  
+var valueFrom="Sm";
+var valueTo=valueFrom +'\uFFFF';
+var index=indexOn[0];  // [{ index: 'lastname', textMapperFN: upperCaseFN } , ... ] 
 
-
-// queryFromTo will return a promise 
-firebaseIndex.queryFromTo( index, valueFrom, valueTo, sortUp)
+fbIdx.queryFromTo(index, valueFrom, valueTo, sortUp)
     .then(function(result){                
-            // using $scope in angular simplest way to wrap it into a  $timeout   
+            // instead of  '$scope.watch' use shorthand '$timeout'   
         $timeout(function(){$scope.result = result;});            
     })
+
+// // Try also 
+// fbIdx.queryFromTo(index, valueFrom, valueTo, sortUp)
+// fbIdx.queryStartAt(index, startAt, limit, sortUp)
+// fbIdx.queryEndAt(index, endAt, limit, sortUp)
+// fbIdx.queryFirst(index, startAt)
+// fbIdx.queryLast(index, endAt)
 
 */
 
     firebase.factory('firebaseIndex', [
-        function () {
-
-            // local var
-            var reference; 
-            var referenceIndex; 
-            var indexOnArray;
+        function (){
             
-            // local func
+            // FACTORY - Return your FirebaseIndex Object with or without 'new'
+            return function (ref, indexOn, refIndex) {
+
+            // Local Vars
+            var reference, 
+                referenceIndex,
+                referenceIndexDefaultName="IDX",
+                indexOnArray,
+                resultArr=[],
+                refChildAdded,
+                refChildRemoved,
+                refChildChanged;
+            
+            // local funcs
             function isFunction(fn){
                 return (typeof fn === 'function');
             }
-            
             function indexHandle(refIndex, indexOn, snapshot, remove) {
-                
+
+                //console.log("indexHandle", indexOn, refIndex.path)
+
+                var idx,
+                    textMapperFN,
+                    textMapperArguments,                    
+                    priority,
+                    snapValue=snapshot.val();  
+
+                if (snapValue === undefined ) {return; }    // ILLEGAL INDEX PROPERTY 
+
+                // iterate indexOn Array 
                 indexOn.forEach(function (ix) {
 
-                    var idx = ix.index;
-                    var textMapperFN= isFunction(ix.textMapper) ? ix.textMapper: function(i){return i;}; 
-                   
-                    var value=snapshot.val();
-                    if (typeof value === 'undefined') {return; }    // ILLEGAL INDEX PROPERTY 
-                    
+                    var value=snapValue;
+
+                    idx = ix.index;
+                    textMapperFN= isFunction(ix.textMapperFN) ? ix.textMapperFN: function(i){return i;}; 
+                    textMapperArguments= ix.textMapperArguments;                     
+
                     /*
                         If your Index is a nested path like { index: 'adress/street' }
-                        then take the value of the last child in the path and extract here the value of 'street'
-                    */
-                    ix.index.trim().split("/").forEach(function(i){
-                        value=value[i];
+                        get the value from the rightest child in the path and extract here the value of 'street'
+                    */                    
+                    ix.index.trim().split("/").forEach(function(i){  
+                        value=value ? value[i] : null;
                     });
-                    
-                    if (typeof value === 'undefined') {return; }    // ILLEGAL INDEX PROPERTY 
-                    
-                    var priority=textMapperFN(value);            
+
+                    if (value === undefined || value === null) {return; }    // ILLEGAL INDEX PROPERTY 
+
+                    priority=textMapperFN(value, textMapperArguments);            
                     if (typeof priority === 'object') {return;}         // JSON OBJECT not allowed for priority (can be solved in textMapperFN)
-                    if (typeof priority === 'boolean') {return; }       // BOOLEAN NOT FOR INDEXING
+                    // if (typeof priority === 'boolean') {return; }       // BOOLEAN NOT FOR INDEXING
                     if (typeof priority === 'function') {return; }      // FUNCTION NOT FOR INDEXING
 
-                    // remove Flag - removes db entry
+                    // remove Flag - removes Dataset 
                     if (remove) {value=null; }
 
                     // ADD  - UPDATE - REMOVE
+                    // console.log("HANDLE IDX:", idx, snapshot.name(),"PRIORITY:", priority, " VALUE:",value, "------------------")
                     refIndex.child(idx).child(snapshot.name()).setWithPriority(value, priority);
 
                 });
             } 
-            
             function queryUp(ref, dataSnapshot){       
-                var result=[];
+
                 dataSnapshot.forEach(function(i){
-                    ref.child(i.name()).once('value', function (dataSnapshot) { 
-                        result.push(dataSnapshot.val());                             
+                    ref.child(i.name()).once('value', function (dataSnapshot) {  
+                            resultArr.push(dataSnapshot.val());                             
                     });
-                });
-                return result;
-            }
-            
+                });                
+                //console.log("### resultArr ", resultArr)                        
+                return resultArr;
+            }    
             function queryDown(ref, dataSnapshot){
-                var result=[];
+
                 dataSnapshot.forEach(function(i){
                     ref.child(i.name()).once('value', function (dataSnapshot) {               
-                        result.unshift(dataSnapshot.val()); 
+                        resultArr.unshift(dataSnapshot.val()); 
                     });
                 });
-                return result;
-            }
-            
-            function queryDo(ref,refIndex ,sort) {
+
+                return resultArr;
+            }               
+            function queryDo(ref, queryRef ,sort) {
+
+                resultArr=[];
 
                 return new Promise(function (resolve, reject) {
-                    refIndex.once('value', function (dataSnapshot) { 
-                        if(sort==false) {resolve(queryDown(ref, dataSnapshot)); return;} //  == accepts all false values 
-                        resolve(queryUp(ref, dataSnapshot));                    
+                    queryRef.once('value', function (dataSnapshot) { 
+                        if(sort==false) {resolve(queryDown(ref, dataSnapshot )); return;} //  == accepts all false values 
+                        resolve(queryUp(ref, dataSnapshot ));                    
                     }, function (errorObject) {
                         reject(errorObject);
                     });
@@ -165,94 +217,122 @@ firebaseIndex.queryFromTo( index, valueFrom, valueTo, sortUp)
                 });
             }        
 
-            // -------------------------------------------------------------------------------------     
-            
+            // if refIndex is undefined generate IDX as Child of your Data with Name 'referenceIndexDefaultName'
+            if(refIndex=== undefined) {refIndex=ref.parent().child(referenceIndexDefaultName);}
+            // set Ref's intern variable             
+            reference=ref;
+            referenceIndex=refIndex;
+            indexOnArray=indexOn;
+ 
             var self = {};
 
-            self.indexOn=function(ref, indexOn, refIndex ){
+            self.indexOn=function(){
                 // ref = new Firebase(data.firebase.../Data);
-                // indexOn = [ {index:lastname, textMapper: toUpperFN}, {index:firstname} ]
+                // indexOn = [ {index:lastname, textMapperFN: toUpperFN}, {index:firstname} ]
                 // refIndex = new Firebase(idx.firebase.../Data/IDX);
+                var firstRun=true;
 
-                // if refIndex undefined generate IDX as Child of your Data
-                if(!refIndex) {refIndex=ref.parent().child("IDX");}
-                // set Ref's intern variable             
-                reference=ref;
-                referenceIndex=refIndex;
-                indexOnArray=indexOn;
-
+                if(refChildAdded || !referenceIndex || !reference) {return;}
+                
                 // set events    
-                reference.limit(1).on('child_added', function (dataSnapshot) {
-                    indexHandle(refIndex, indexOn, dataSnapshot);
+                refChildAdded=reference.limit(1).on('child_added', function (dataSnapshot) {
+                    if(firstRun===false) {indexHandle(refIndex, indexOn, dataSnapshot); }
+                    firstRun=false;
                 });
-
-                reference.on('child_removed', function (dataSnapshot) {
+                   
+                refChildRemoved=reference.on('child_removed', function (dataSnapshot) {
                     indexHandle(refIndex, indexOn, dataSnapshot,true);
                 });
 
-                reference.on('child_changed', function (dataSnapshot) {
+                refChildChanged=reference.on('child_changed', function (dataSnapshot) {
                     indexHandle(refIndex, indexOn, dataSnapshot);
                 });
 
             };
 
+            self.indexOff=function(){
+
+                 if(!referenceIndex || !reference) {return;}
+
+                // set events    
+                reference.off('child_added', refChildAdded);
+
+                reference.off('child_removed', refChildRemoved);
+
+                reference.off('child_changed', refChildChanged);
+
+                refChildAdded=null;
+                
+            };
+
+            self.indexDelete=function(){
+
+                if(!referenceIndex || !reference) {return;}                
+            
+                referenceIndex.set(null);
+            
+            };
+
             self.indexRecreate=function(){
                 // ref = new Firebase(data.firebase.../Data);
-                // indexOn = [ {index:lastname, textMapper: toUpperFN}, {index:firstname} ]
+                // indexOn = [ {index:lastname, textMapperFN: toUpperFN}, {index:firstname} ]
                 // refIndex = new Firebase(idx.firebase.../Data/IDX);
 
                 if(!referenceIndex || !reference) {return;}
 
                 referenceIndex.set(null, function(){
                     //     set events    
-                    reference.on('child_added', function (dataSnapshot) {
+                    var refRecreate=reference.on('child_added', function (dataSnapshot) {
                         indexHandle(referenceIndex, indexOnArray, dataSnapshot);
                     });
-
+                    // switch off recreation Reference
+                    reference.off('child_added', refRecreate);  
+                
                 });
 
             };
-
             
-            
+            // QUERY
+                
             self.queryFromTo = function(index, startAt, endAt, sort ) {
 
-                var textFN= isFunction(index.textMapper) ? index.textMapper: function(i){return i;}; 
-                var queryRef=referenceIndex.child(index.index).startAt(textFN(startAt)).endAt(textFN(endAt));     
-                
-                return queryDo(reference, queryRef,sort); // Promise
+                var textFN= isFunction(index.textMapperFN) ? index.textMapperFN: function(i){return i;}; 
+                var queryRef=referenceIndex.child(index.index).startAt(textFN(startAt)).endAt(textFN(endAt) );     
+                return queryDo(reference, queryRef, sort); // Promise
             };
 
             self.queryStartAt = function(index, startAt, limit, sort) {            
 
-                var textFN= isFunction(index.textMapper) ? index.textMapper: function(i){return i;}; 
+                var textFN= isFunction(index.textMapperFN) ? index.textMapperFN: function(i){return i;}; 
                 var queryRef=referenceIndex.child(index.index).startAt(textFN(startAt)).limit(limit);            
                 return queryDo(reference, queryRef,sort);  // Promise
             };
 
             self.queryEndAt = function(index, endAt, limit, sort) {            
 
-                var textFN= isFunction(index.textMapper) ? index.textMapper: function(i){return i;}; 
+                var textFN= isFunction(index.textMapperFN) ? index.textMapperFN: function(i){return i;}; 
                 var queryRef=referenceIndex.child(index.index).endAt(textFN(endAt)).limit(limit);            
                 return queryDo(reference, queryRef,sort); // Promise
             };
 
             self.queryFirst = function(index, startAt) {            
 
-                var textFN= isFunction(index.textMapper) ? index.textMapper: function(i){return i;}; 
+                var textFN= isFunction(index.textMapperFN) ? index.textMapperFN: function(i){return i;}; 
                 var queryRef=referenceIndex.child(index.index).startAt(textFN(startAt)).limit(1);            
                 return queryDo(reference, queryRef,true); // Promise
             };
 
             self.queryLast = function(index, endAt) {            
 
-                var textFN= isFunction(index.textMapper) ? index.textMapper: function(i){return i;}; 
+                var textFN= isFunction(index.textMapperFN) ? index.textMapperFN: function(i){return i;}; 
                 var queryRef=referenceIndex.child(index.index).endAt(textFN(endAt)).limit(1);            
                 return queryDo(reference, queryRef,true); // Promise
             };
 
             return self;
 
-    }]);
+        };
 
+     }]);
+        
 }());
